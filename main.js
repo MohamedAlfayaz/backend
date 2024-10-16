5079c60ec25714c42842dfeab55a290817f385a8
MohamedAlfayaz <alfayaz23112004@gmail.com>
2024-10-16 15:50:19 +0530

    route structure

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 import express from "express";
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.json({msg:'Hello World!'})
-})
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello World!" });
+});
+
+//CURD funationality
+// R - For Reading
+app.get("/movies", () => {});
+
+// C - For Createing a Movies
+app.post("/movies", () => {});
+
+// U -  For Updating a Movies
+app.put("/movies/:id", () => {});
+
+// D - For Deleteing movies
+app.delete("/movies/:id", () => {});
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
-})
+  console.log(`Example app listening on http://localhost:${port}`);
+});
